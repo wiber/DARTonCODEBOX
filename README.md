e753618a4fc3e77a6213547b4c2d679a935cb20e
codebox workspace user <elias@thewibe.net>
2014-08-24 07:05:06 +0000

    added README.me

---
+++ README.md
@@ -0,0 +1,3 @@
+// pub perve --hostname=0.0.0.0 --port=2000
+// add /index.html
+// http://web-2000.d1c012cf-3476-402a-bbeb-7ab3c57a72fb.vm1.dynobox.io/index.html
--- web/helloworld.dart
+++ web/helloworld.dart
@@ -14,6 +14,3 @@ void reverseText(MouseEvent event) {
   }
   querySelector("#sample_text_id").text = buffer.toString();
 }
-// pub perve --hostname=0.0.0.0 --port=2000
-// add /index.html
-// http://web-2000.d1c012cf-3476-402a-bbeb-7ab3c57a72fb.vm1.dynobox.io/index.html
